daef8d83bd469e323aaba33c2b9fafb16c8b7007
Natalia Goncharenko <muratovan0921@gmail.com>
2025-04-22 19:15:34 +0300

    add game selection menu

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,21 @@
 package hexlet.code;
 
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.sayHi();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the game number and press Enter.\n"
+                + "1 - Greet\n"
+                + "0 - Exit");
+
+        int num = scanner.nextInt();
+
+        System.out.println("Your choice: " + num);
+        if (num != 0) {
+            Cli.sayHi();
+        }
     }
 }

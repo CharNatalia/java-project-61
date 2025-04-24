f6286d65957b0dd358b9b933fc725b590fedd585
Natalia Goncharenko <muratovan0921@gmail.com>
2025-04-24 18:30:11 +0300

    refactor package layout and class Even

---
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -0,0 +1,38 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+import hexlet.code.User;
+
+import java.util.Random;
+
+public class Even {
+
+    public static void game(User user) {
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+
+        Random random = new Random();
+
+        var victoryCounter = 0;
+        var answerIsRight = true;
+
+        while (victoryCounter != 3 && answerIsRight) {
+            int randomNum = random.nextInt(100);
+
+            String newAnswer = Engine.askQuestion(Integer.toString(randomNum));
+
+            String rightAnswer = isEven(randomNum);
+
+            answerIsRight = Engine.answerCheck(rightAnswer, newAnswer, user);
+
+            victoryCounter++;
+        }
+        Engine.isWinner(user, victoryCounter);
+    }
+
+    public static String isEven(int num) {
+        if (num % 2 == 0) {
+            return "yes";
+        }
+        return "no";
+    }
+}

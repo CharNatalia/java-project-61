bb0c0a597c75f0124375a03ed1c707bfbb170f20
Natalia Goncharenko <muratovan0921@gmail.com>
2025-04-30 16:28:40 +0300

    refactor the game "Even"

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -10,23 +10,28 @@ public class Even {
     public static void play() {
         String rules = "Answer 'yes' if the number is even, otherwise answer 'no'.";
 
-
         String[][] questionsAndAnswers = new String[ROW_LENGTH][COLUMN_LENGTH];
 
-
         for (var i = 0; i < COLUMN_LENGTH; i++) {
-            int randomNum = Utils.getRandomNum();
-
-            questionsAndAnswers[0][i] = String.format("%d", randomNum);
-            questionsAndAnswers[1][i] = getRightAnswer(randomNum);
+            var tmp = 0;
+            for (var oneRound : generateRoundData()) {
+                questionsAndAnswers[tmp][i] = oneRound;
+                tmp++;
+            }
         }
         Engine.answerCheck(questionsAndAnswers, rules);
     }
 
-    public static String getRightAnswer(int num) {
-        if (num % 2 == 0) {
-            return "yes";
-        }
-        return "no";
+    public static String[] generateRoundData() {
+        String[] oneRoundData = new String[2];
+        int randomNum = Utils.getRandomNum();
+
+        oneRoundData[0] = String.format("%d", randomNum);
+        oneRoundData[1] = isEven(randomNum) ? "yes" : "no";
+        return oneRoundData;
+    }
+
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
     }
 }

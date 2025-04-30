5a0bfd5127d9463c21e153463bcbcb44560f4684
Natalia Goncharenko <muratovan0921@gmail.com>
2025-04-30 16:28:30 +0300

    refactor the game "Calc"

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -15,21 +15,31 @@ public class Calc {
         String[][] questionsAndAnswers = new String[ROW_LENGTH][COLUMN_LENGTH];
 
         for (var i = 0; i < COLUMN_LENGTH; i++) {
-            int randomNum1 = Utils.getRandomNum();
-            int randomNum2 = Utils.getRandomNum();
-            char randomOperator = operators[Utils.getRandomNum(0, operators.length)];
-
-            questionsAndAnswers[0][i] = String.format("%d %c %d", randomNum1, randomOperator, randomNum2);
-            questionsAndAnswers[1][i] = getRightAnswer(randomNum1, randomNum2, randomOperator);
+            var tmp = 0;
+            for (var oneRound : generateRoundData(operators)) {
+                questionsAndAnswers[tmp][i] = oneRound;
+                tmp++;
+            }
         }
         Engine.answerCheck(questionsAndAnswers, rules);
     }
 
-    public static String getRightAnswer(int randomNum1, int randomNum2, char operator) {
+    public static String[] generateRoundData(char[] operators) {
+        String[] oneRoundData = new String[2];
+        int randomNum1 = Utils.getRandomNum();
+        int randomNum2 = Utils.getRandomNum();
+        char randomOperator = operators[Utils.getRandomNum(0, operators.length)];
+
+        oneRoundData[0] = String.format("%d %c %d", randomNum1, randomOperator, randomNum2);
+        oneRoundData[1] = Integer.toString(getCalculated(randomNum1, randomNum2, randomOperator));
+        return oneRoundData;
+    }
+
+    public static int getCalculated(int randomNum1, int randomNum2, char operator) {
         return switch (operator) {
-            case '+' -> Integer.toString(randomNum1 + randomNum2);
-            case '*' -> Integer.toString(randomNum1 * randomNum2);
-            case '-' -> Integer.toString(randomNum1 - randomNum2);
+            case '+' -> randomNum1 + randomNum2;
+            case '*' -> randomNum1 * randomNum2;
+            case '-' -> randomNum1 - randomNum2;
             default -> throw new UnsupportedOperationException(
                     String.format("Operation %c is not supported", operator)
             );

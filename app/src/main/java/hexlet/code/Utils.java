2ebdce47d1aef019df99e909952c5a13163f8d46
Natalia Goncharenko <muratovan0921@gmail.com>
2025-05-04 14:48:10 +0300

    fix naming in Utils class

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -6,11 +6,11 @@ public class Utils {
     private static final Random RANDOM = new Random();
     private static final int MAX_NUMBER_RANGE = 101;
 
-    public static int getRandomNum(int origin, int bound) {
+    public static int generateRandomNumber(int origin, int bound) {
         return RANDOM.nextInt(origin, bound);
     }
 
-    public static int getRandomNum() {
+    public static int generateRandomNumber() {
         return RANDOM.nextInt(MAX_NUMBER_RANGE);
     }
 }
